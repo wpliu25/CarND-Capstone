04a63ba1a032e735652a99a47026ce13e0d37830
Denis Moroz <moroz.denis@gmail.com>
2017-11-08 13:46:26 +0300

    First version for waypoint updater

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from itertools import islice, cycle
 
 import rospy
 from geometry_msgs.msg import PoseStamped
@@ -21,7 +22,7 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 20  # Number of waypoints we will publish. You can change this number
 
 
 class WaypointUpdater(object):
@@ -33,20 +34,24 @@ class WaypointUpdater(object):
 
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
-
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
 
+        self.waypoints = None
+        self.current_pos = None
+
         rospy.spin()
 
     def pose_cb(self, msg):
-        # TODO: Implement
-        pass
+        self.current_pos = msg.pose
+        rospy.loginfo("WaypointUpdater: current car position %s", self.current_pose)
+        self.publish_waypoints()
 
     def waypoints_cb(self, waypoints):
-        # TODO: Implement
-        pass
+        self.waypoints = waypoints.waypoints
+        rospy.loginfo("WaypointUpdater: received waypoints")
+        self.publish_waypoints()
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
@@ -69,6 +74,40 @@ class WaypointUpdater(object):
             dist += dl(waypoints[wp1].pose.pose.position, waypoints[i].pose.pose.position)
             wp1 = i
         return dist
+
+    def publish_waypoints(self):
+        if self.waypoints is None or self.current_pos is None:
+            return
+
+        car_x = self.current_pos.position.x
+        car_y = self.current_pos.position.y
+
+        nearest_wp_idx = None
+        nearest_distance = None
+
+        for i in range(len(self.waypoints)):
+            wp = self.waypoints[i]
+            wp_x = wp.position.x
+            wp_y = wp.position.y
+
+            distance = math.sqrt((car_x - wp_x)**2 + (car_y - wp_y)**2)
+
+            if nearest_distance is None:
+                nearest_distance = distance
+                nearest_wp_idx = i
+            else:
+                if distance < nearest_distance:
+                    nearest_distance = distance
+                    nearest_wp_idx = i
+
+        final_waypoints = list(islice(cycle(self.waypoints), nearest_wp_idx, nearest_wp_idx + LOOKAHEAD_WPS - 1))
+
+        lane = Lane()
+        lane.waypoints = final_waypoints
+        lane.header.frame_id = '/world'
+        lane.header.stamp = rospy.Time(0)
+
+        self.final_waypoints_pub.publish(lane)
 
 
 if __name__ == '__main__':
