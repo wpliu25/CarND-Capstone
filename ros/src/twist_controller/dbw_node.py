0c326bcaa9a9638e06c0c360495c8d344ef38967
Denis Moroz <moroz.denis@gmail.com>
2017-11-09 13:59:29 +0300

    Update parameters

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -86,18 +86,18 @@ class DBWNode(object):
             rate.sleep()
 
     def publish(self, throttle, brake, steer):
-        if throttle != 0:
-            tcmd = ThrottleCmd()
-            tcmd.enable = True
-            tcmd.pedal_cmd_type = ThrottleCmd.CMD_PERCENT
-            tcmd.pedal_cmd = throttle
-            self.throttle_pub.publish(tcmd)
-        else:
-            bcmd = BrakeCmd()
-            bcmd.enable = True
-            bcmd.pedal_cmd_type = BrakeCmd.CMD_TORQUE
-            bcmd.pedal_cmd = brake
-            self.brake_pub.publish(bcmd)
+        tcmd = ThrottleCmd()
+        tcmd.enable = True
+        tcmd.pedal_cmd_type = ThrottleCmd.CMD_PERCENT
+        tcmd.pedal_cmd = throttle
+        self.throttle_pub.publish(tcmd)
+
+
+        bcmd = BrakeCmd()
+        bcmd.enable = True
+        bcmd.pedal_cmd_type = BrakeCmd.CMD_TORQUE
+        bcmd.pedal_cmd = brake
+        self.brake_pub.publish(bcmd)
 
         scmd = SteeringCmd()
         scmd.enable = True
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -6,8 +6,8 @@ from pid import PID
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
-MAX_SPEED = 30
-MIN_SPEED = 0.001
+MAX_THROTTLE = 1
+MIN_THROTTLE = 0.001
 
 
 class Controller(object):
@@ -16,9 +16,9 @@ class Controller(object):
                                          kwargs['min_speed'], kwargs['max_lat_accel'],
                                          kwargs['max_steer_angle'])
 
-        self.low_pass_filter = LowPassFilter(0.11, 0.12)
+        self.low_pass_filter = LowPassFilter(0.2, 0.1)
 
-        self.throttle_control = PID(0.3, 0.0001, MIN_SPEED, MAX_SPEED)
+        self.throttle_control = PID(0.3, 0.0001, 0, -MAX_THROTTLE, MAX_THROTTLE)
 
         self.last_run_ts = None
 
