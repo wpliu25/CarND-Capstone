45215de8c3fcfccd9838f00615ea999f1d4413a8
student <rainer.valek@web.de>
2017-11-25 04:57:39 -0800

    removed darkflow

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -5,15 +5,15 @@ import os
 import tensorflow as tf
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils
-from darkflow.net.build import TFNet
 import rospy
 import time
 import cv2
 
 class TLClassifier(object):
     def __init__(self, debug=False, simulator=False):
+        
         self.debug = debug
-        self.tryDarkflow = False
+
         if self.debug:
             cv2.namedWindow('image', cv2.WINDOW_NORMAL)
             cv2.resizeWindow('image', 500, 500)
@@ -22,71 +22,51 @@ class TLClassifier(object):
         self.current_light = TrafficLight.UNKNOWN
         cwd = os.path.dirname(os.path.realpath(__file__))
 
-        if not self.tryDarkflow:
-
-            # load different graphs dep on config, sim or real
-            if self.simulator:
-                CKPT = cwd + '/graphs/sim/frozen_inference_graph.pb'
-            else:
-                CKPT = cwd + '/graphs/real/frozen_inference_graph.pb'
-
-            label_map = label_map_util.load_labelmap(
-                cwd + '/graphs/label_map.pbtxt')
-            categories = label_map_util.convert_label_map_to_categories(label_map,
-                                                                        max_num_classes=14,
-                                                                        # 14 from bosch dataset
-                                                                        use_display_name=True)
-
-            # get label map categories
-            self.category_index = label_map_util.create_category_index(categories)
-            print self.category_index
-
-            # TF graph set up
-            self.classifer_graph = tf.Graph()
-            config = tf.ConfigProto()
-            config.gpu_options.allow_growth = True
-
-            with self.classifer_graph.as_default():
-                od_graph_def = tf.GraphDef()
-
-                with tf.gfile.GFile(CKPT, 'rb') as fid:
-                    serialized_graph = fid.read()
-                    od_graph_def.ParseFromString(serialized_graph)
-                    tf.import_graph_def(od_graph_def, name='')
-
-                self.sess = tf.Session(graph=self.classifer_graph, config=config)
-
-            self.image_tensor = self.classifer_graph.get_tensor_by_name(
-                'image_tensor:0')
-            self.detection_boxes = self.classifer_graph.get_tensor_by_name(
-                'detection_boxes:0')
-            self.detection_scores = self.classifer_graph.get_tensor_by_name(
-                'detection_scores:0')
-            self.detection_classes = self.classifer_graph.get_tensor_by_name(
-                'detection_classes:0')
-            self.num_detections = self.classifer_graph.get_tensor_by_name(
-                'num_detections:0')
-
-            rospy.loginfo("*** TF classifer {} loaded! ***".format(CKPT))
-
+        # load different graphs dep on config, sim or real
+        if self.simulator:
+            CKPT = cwd + '/graphs/sim/frozen_inference_graph.pb'
         else:
+            CKPT = cwd + '/graphs/real/frozen_inference_graph.pb'
 
-            use_gpu = False;
+        label_map = label_map_util.load_labelmap(
+            cwd + '/graphs/label_map.pbtxt')
+        categories = label_map_util.convert_label_map_to_categories(label_map,
+                                                                    max_num_classes=14,
+                                                                    # 14 from bosch dataset
+                                                                    use_display_name=True)
 
-            cwd = os.path.dirname(os.path.realpath(__file__))
+        # get label map categories
+        self.category_index = label_map_util.create_category_index(categories)
+        print self.category_index
 
-            options = {"model": os.path.join(cwd, "cfg/yolo_bosch.cfg"),
-                       "labels": os.path.join(cwd, "labels.txt"),
-                       "metaload": os.path.join(cwd, "ckpt/yolo_bosch-80000.meta"),
-                       "summary": None,
-                       "threshold": 0.1}
+        # TF graph set up
+        self.classifer_graph = tf.Graph()
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
 
-            if use_gpu:
-                options["gpu"] = 1.0
+        with self.classifer_graph.as_default():
+            od_graph_def = tf.GraphDef()
 
-            rospy.loginfo("YOLO classifier loaded")
+            with tf.gfile.GFile(CKPT, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
 
-            self.tfnet = TFNet(options)
+            self.sess = tf.Session(graph=self.classifer_graph, config=config)
+
+        self.image_tensor = self.classifer_graph.get_tensor_by_name(
+            'image_tensor:0')
+        self.detection_boxes = self.classifer_graph.get_tensor_by_name(
+            'detection_boxes:0')
+        self.detection_scores = self.classifer_graph.get_tensor_by_name(
+            'detection_scores:0')
+        self.detection_classes = self.classifer_graph.get_tensor_by_name(
+            'detection_classes:0')
+        self.num_detections = self.classifer_graph.get_tensor_by_name(
+            'num_detections:0')
+
+        rospy.loginfo("*** TF classifer {} loaded! ***".format(CKPT))
+
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -99,72 +79,46 @@ class TLClassifier(object):
         if self.debug:
             tic = time.time()
 
-        if not self.tryDarkflow:
+        self.current_light = TrafficLight.UNKNOWN
+        image_np_expanded = np.expand_dims(image, axis=0)
 
-            self.current_light = TrafficLight.UNKNOWN
-            image_np_expanded = np.expand_dims(image, axis=0)
+        # run detection
+        with self.classifer_graph.as_default():
+            (boxes, scores, classes, num) = self.sess.run(
+                [self.detection_boxes, self.detection_scores,
+                 self.detection_classes, self.num_detections],
+                feed_dict={self.image_tensor: image_np_expanded})
 
-            # run detection
-            with self.classifer_graph.as_default():
-                (boxes, scores, classes, num) = self.sess.run(
-                    [self.detection_boxes, self.detection_scores,
-                     self.detection_classes, self.num_detections],
-                    feed_dict={self.image_tensor: image_np_expanded})
+        boxes = np.squeeze(boxes)
+        scores = np.squeeze(scores)
+        classes = np.squeeze(classes).astype(np.int32)
 
-            boxes = np.squeeze(boxes)
-            scores = np.squeeze(scores)
-            classes = np.squeeze(classes).astype(np.int32)
+        if self.debug:
+            rospy.loginfo('classes: %s \n scores %s ' % (classes[:5], scores[:5]))
 
-            if self.debug:
-                rospy.loginfo('classes: %s \n scores %s ' % (classes[:5], scores[:5]))
+        # light color prediction
+        min_score_thresh = .3
+        for i in range(boxes.shape[0]):
+            if scores is None or scores[i] > min_score_thresh:
+                class_name = self.category_index[classes[i]]['name']
+                if class_name == 'Red':
+                    self.current_light = TrafficLight.RED
+                elif class_name == 'Green':
+                    self.current_light = TrafficLight.GREEN
+                elif class_name == 'Yellow':
+                    self.current_light = TrafficLight.YELLOW
+                break # Here we go we found best match!
 
-            # light color prediction
-            min_score_thresh = .3
-            for i in range(boxes.shape[0]):
-                if scores is None or scores[i] > min_score_thresh:
-                    class_name = self.category_index[classes[i]]['name']
-                    if class_name == 'Red':
-                        self.current_light = TrafficLight.RED
-                    elif class_name == 'Green':
-                        self.current_light = TrafficLight.GREEN
-                    elif class_name == 'Yellow':
-                        self.current_light = TrafficLight.YELLOW
-                    break # Here we go we found best match!
-
-            # Visualization of the results of a detection.
-            visualization_utils.visualize_boxes_and_labels_on_image_array(
-                image, boxes, classes, scores,
-                self.category_index,
-                use_normalized_coordinates=True,
-                line_thickness=8)  # For visualization topic output
-            if self.debug:
-                self._current_image = image
-                cv2.imshow('image', image)
-                cv2.waitKey(1)
-
-        else:
-
-            result = self.tfnet.return_predict(image)
-
-            if not result:
-                self.current_light = TrafficLight.UNKNOWN
-
-            # extract results
-
-            red     = [l for l in result if l["label"] == "Red"]
-            green   = [l for l in result if l["label"] == "Green"]
-            yellow  = [l for l in result if l["label"] == "Yellow"]
-            off     = [l for l in result if l["label"] == "off"]
-
-            # definitely break if there is any red light
-            if red:
-                self.current_light = TrafficLight.RED
-
-            if yellow:
-                self.current_light = TrafficLight.YELLOW
-
-            if green:
-                self.current_light = TrafficLight.GREEN
+        # Visualization of the results of a detection.
+        visualization_utils.visualize_boxes_and_labels_on_image_array(
+            image, boxes, classes, scores,
+            self.category_index,
+            use_normalized_coordinates=True,
+            line_thickness=8)  # For visualization topic output
+        if self.debug:
+            self._current_image = image
+            cv2.imshow('image', image)
+            cv2.waitKey(1)
 
         if self.debug:
             toc = time.time()
